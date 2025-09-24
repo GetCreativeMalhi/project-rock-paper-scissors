1ad19af2ac4a62f541bc6af330073d33b5262c32
GetCreativeMalhi <108103415+GetCreativeMalhi@users.noreply.github.com>
2025-09-24 11:31:14 +1000

    feat: add Rock, Paper, Scissor btns play a round
    - add UI buttons for user to play a round(event
    listener).
    - display results in text(DOM).
    - code refactored to play the game in the browser.

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-
-
-
-
-// FUNCTION getComputerChoice
-//     RETURN Math.random 
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 100);
     if (num <= 33) {
@@ -18,57 +12,53 @@ function getComputerChoice() {
     }
 }
 
+
+
 // FUNCTION getHumanChoice
     // PROMPT user to enter choice
-function getHumanChoice() {
-    const choice = prompt("Make your choice: Rock, Paper, or Scissors").toLowerCase();
+function getHumanChoice(e) {
+    // const choice = prompt("Make your choice: Rock, Paper, or Scissors").toLowerCase();
     // console.log(`Your Choice: ${choice}`);
-    return choice;
+    return e.target.textContent.toLowerCase();
+    
+}
+
+function playRound(humanChoice, computerChoice) {
+    let drawScore = 0;
+    let humanScore = 0;
+    let computerScore = 0;
+    console.log(`Your Chose: ${humanChoice}, Computer Chose: ${computerChoice}`)
+    if (humanChoice === computerChoice){
+        drawScore++;
+        return [drawScore, humanScore, computerScore];
+    }else if ((humanChoice === "rock" && computerChoice === "scissors") || 
+        (humanChoice === "scissors" && computerChoice === "paper") ||
+        (humanChoice === "paper" && computerChoice ==="rock")){
+        humanScore++;
+        return [drawScore, humanScore, computerScore];
+    } else {
+        computerScore++;
+       return [drawScore, humanScore, computerScore];
+    }
+    // if (drawScore != 5 || humanScore != 5 || computerScore != 5){
+    //     const div = document.querySelector('result');
+    //     div.textContent = "🎮 Game is on... make your move!"
+    // }else if(drawScore === 5 || humanScore === 5 || computerScore === 5){
+    //     div.textContent = "Game Over"
+    // }
+    // const result = document.querySelector('.resultPara');
+    // result.textContent = `Round complete: Draw Score: ${drawScore} Human Score: ${humanScore} Computer Score: ${computerScore}`;
+            
 }
 
 // FUNCTION playGame function that calculates score for five games and declares winner
-function playGame () {
-    // store scores for human and computer
-    let humanScore = 0;
-    let computerScore = 0;
-    let drawScore = 0;
-    let humanSelection;
-    let computerSelection;
 
-    //  FUNCTION playRound here takes human and computer choices as arguments 
-    function playRound(humanChoice, computerChoice) {
-        console.log(`Your Chose: ${humanChoice}, Computer Chose: ${computerChoice}`)
-        if (humanSelection === computerSelection){
-            drawScore++;
-            console.log("Thats Draw, Play again");
-        }else if ((humanSelection === "rock" && computerSelection === "scissors") || 
-                (humanSelection === "scissors" && computerSelection === "paper") ||
-                (humanSelection === "paper" && computerSelection ==="rock")){
-            humanScore++;
-            console.log(`You win! ${humanSelection} beats ${computerChoice}`);
-        } else {
-            computerScore++;
-            console.log(`You lose! ${computerSelection} beats ${humanSelection}`);
-        }
-    }
-    // console.log(`Your Score: ${humanScore}, Computer Score: ${computerScore}, Draw Score ${drawScore}`)
-
-    for (let i = 1; i <= 5; i++){
-       // FUNCTIONs are invoked here and values are stored in the variables
-       console.log(`********** Begin Round ${i} **********`); 
-       humanSelection = getHumanChoice();
-        computerSelection = getComputerChoice();  
-        playRound(humanSelection, computerSelection);
-        console.log(`Your Score: ${humanScore}, Computer Score: ${computerScore}, Draw Score: ${drawScore}`)
-    }
-    if (humanScore > computerScore){
-        console.log("You have won the game")
-    }else {
-        console.log("you lost the game")
-    }
-}
-
-
-
-playGame();
-
+const buttons = document.querySelectorAll('button');
+    buttons.forEach(button =>{
+        button.addEventListener('click', (e) => {
+        const [draw, human, computer] = playRound(getComputerChoice(), getHumanChoice(e));
+        
+        const result = document.querySelector('.resultPara');
+        result.textContent = `Round complete: Draw Score: ${draw} Human Score: ${human} Computer Score: ${computer}`;
+        });
+    });
